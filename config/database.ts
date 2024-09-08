cede9a687eea382a831274c956c8ff98326760b4
Nguyen Van A <nguyenvana@example.com>
2024-09-08 10:07:29 +0700

    update code

---
+++ config/database.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME, // tên database
+    process.env.DATABASE_USERNAME, // username đăng nhập
+    process.env.DATABASE_PASSWORD, // mật khẩu
+    {
+        host: process.env.DATABASE_HOST, // link hosting
+        dialect: 'mysql'
+    }
+);
+
+
+sequelize.authenticate().then(() => {
+    console.log('Connect Success!');
+}).catch((error) => {
+    console.error('Connect Error: ', error);
+});
+
+export default sequelize
--- index.ts
+++ index.ts
@@ -1,10 +1,29 @@
+import { like } from './../music-app-ts/controllers/client/song.controller';
 import express, { Express, Request, Response } from "express";
+import sequelize from "./config/database";
+import dotenv from "dotenv";
+
+import Tour from "./models/tour.model";
+
+dotenv.config();
+
+sequelize;
 
 const app: Express = express(); 
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Trang chủ");
+// cài pug
+app.set('views', "./views");
+app.set('view engine', 'pug');
+
+app.get("/tours", async (req: Request, res: Response) => {
+    const tours = await Tour.findAll({
+        raw: true,
+    });
+
+    res.render("client/pages/tours/index", {
+        tours: tours
+    });
 });
 
 app.listen(port, () => {
